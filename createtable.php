a0f277ec651df3e8e71ffbc042cfbdbb4550b5b9
Thundernator <paulus131313@yahoo.de>
2013-11-20 19:16:15 +0100

    users table fix

--- createtable.php
+++ createtable.php
@@ -14,7 +14,7 @@ $sql="
    
       id INT NOT NULL AUTO_INCREMENT PRIMARY KEY ,
 	  
-	  name VARCHAR( 100 ) NOT NULL ,
+	  user VARCHAR( 100 ) NOT NULL ,
    
       password VARCHAR( 100 ) NOT NULL 
    
