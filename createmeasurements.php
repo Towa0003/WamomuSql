d1586f360ba15aae3cdc9bf969cab8bb3dab81f4
Christian <eaglechris@hotmail.de>
2013-11-23 12:06:50 +0100

    Noma hochkommas weggemacht

--- createmeasurements.php
+++ createmeasurements.php
@@ -12,30 +12,30 @@ $sql="
 
 
 -- -----------------------------------------------------
--- Table `wamomu`.`measurements`
+-- Table wamomu.measurements
 -- -----------------------------------------------------
 CREATE TABLE measurements (
   mvalue INT NOT NULL,
   date DECIMAL NOT NULL,
   users_id INT NOT NULL,
   PRIMARY KEY ( users_id ),
-  CONSTRAINT fk_measurements_users`
+  CONSTRAINT fk_measurements_users
     FOREIGN KEY (users_id)
     REFERENCES wamomu.users ( id )
     ON DELETE NO ACTION
     ON UPDATE NO ACTION)
 
 -- -----------------------------------------------------
--- Table `wamomu`.`meals`
+-- Table wamomu.meals
 -- -----------------------------------------------------
 CREATE TABLE meals (
-  `mealkind` INT NOT NULL,
-  `date` DECIMAL NOT NULL,
-  `users_id` INT NOT NULL,
-  PRIMARY KEY (`users_id`),
-  CONSTRAINT `fk_meals_users1`
-    FOREIGN KEY (`users_id`)
-    REFERENCES `wamomu`.`users` (`id`)
+  mealkind INT NOT NULL,
+  date DECIMAL NOT NULL,
+  users_id INT NOT NULL,
+  PRIMARY KEY (users_id),
+  CONSTRAINT fk_meals_users1
+    FOREIGN KEY (users_id)
+    REFERENCES wamomu.users (id)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION)
 ";
