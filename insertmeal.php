4baa14a480cc56b24cf17fc7a393eb67f61143de
Thundernator <paulus131313@yahoo.de>
2013-11-23 12:38:25 +0100

    insertmeal added

---
+++ insertmeal.php
@@ -0,0 +1,22 @@
+
+
+<?php
+$con=mysqli_connect("localhost","wamomu","wamomu","wamomu");
+// Check connection
+if (mysqli_connect_errno())
+  {
+  echo "Failed to connect to MySQL: " . mysqli_connect_error();
+  }
+
+$sql="INSERT INTO meals (mealkind, date, users_id)
+VALUES ('Frühstück', 20.11,1)";
+
+if (!mysqli_query($con,$sql))
+  {
+  die('Error: ' . mysqli_error($con));
+  }
+echo " record added";
+
+mysqli_close($con);
+?>
+
