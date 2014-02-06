76b02113464b7d4ae8b4ca27fcf772e360f07611
Christian <eaglechris@hotmail.de>
2014-02-06 19:31:17 +0100

    changed and added new values to meals and measurement

--- insertmeal.php
+++ insertmeal.php
@@ -7,14 +7,27 @@ if (mysqli_connect_errno())
   }
 
 $sql="INSERT INTO meals (meal_id, mealkind, meal, date, time, users_id) 
-VALUES ('', 'Morgens', 'Nutellabrot', '2013-10-01', '11:10', 1),
-('', 'Mittag', 'Gulasch', '2013-10-01', '15:30', 1),
-('', 'Abend', 'Ei, Butterbrot', '2013-10-01', '18:20', 1),
-('', 'Morgens', 'Schinkenbrot', '2013-10-02', '10:00', 1),
-('', 'Mittag', 'Pizza', '2013-10-02', '13:50', 1),
-('', 'Abend', 'Kaesebrot', '2013-10-02', '18:30', 1),
-('', 'Morgens', 'Muesli', '2013-10-03', '11:30', 1)";
-// UPDATE meals SET meals.mealkind='Mittag' where users_id=1
+VALUES ('', 'Morgens', 'Nutellabrot', '2014-01-01', '11:28', 1),
+('', 'Morgens', 'Gulasch', '2014-01-03', '11:30', 1),
+('', 'Morgens', 'Ei, Butterbrot', '2014-01-05', '11:18', 1),
+('', 'Morgens', 'Schinkenbrot', '2014-01-07', '11:55', 1),
+('', 'Morgens', 'Pizza', '2014-01-09', '11:20', 1),
+('', 'Morgens', 'Kaesebrot', '2014-01-11', '12:10', 1),
+('', 'Morgens', 'Muesli', '2014-01-13', '12:04', 1),
+('', 'Morgens', 'Gulasch', '2014-01-15', '11:54', 1),
+('', 'Morgens', 'Ei, Butterbrot', '2014-01-17', '11:30', 1),
+('', 'Morgens', 'Schinkenbrot', '2014-01-19', '11:20', 1),
+('', 'Morgens', 'Pizza', '2014-01-21', '11:10', 1),
+('', 'Morgens', 'Kaesebrot', '2014-01-23', '11:30', 1),
+('', 'Morgens', 'Muesli', '2014-01-25', '11:30', 1),
+('', 'Morgens', 'Gulasch', '2014-01-27', '11:20', 1),
+('', 'Morgens', 'Ei, Butterbrot', '2014-01-29', '11:13', 1),
+('', 'Morgens', 'Schinkenbrot', '2014-01-31', '11:30', 1),
+('', 'Morgens', 'Pizza', '2014-02-02', '11:23', 1),
+('', 'Morgens', 'Kaesebrot', '2014-02-04', '11:10', 1),
+('', 'Morgens', 'Muesli', '2014-02-06', '11:30', 1),
+('', 'Morgens', 'Muesli', '2014-02-08', '11:20', 1)";
+// UPDATE meals SET meals.mealkind='Morgens' where users_id=1
 // INSERT INTO meals (mealkind, date, users_id) VALUES ('Frühstück', 20.11, 1)
 // SET CHAR SET 'utf8'
 if (!mysqli_query($con,$sql))
